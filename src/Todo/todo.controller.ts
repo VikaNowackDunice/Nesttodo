229d2906c7ff5bf6718eb4b0fe413fa8526f0c85
Milanavika <v.novak@nv.dunice.net>
2024-03-26 14:29:10 +0300

    corrected

--- src/Todo/todo.controller.ts
+++ src/Todo/todo.controller.ts
@@ -1,6 +1,5 @@
 import { TodoService } from "./todo.service";
 import { Controller, Get, Post, Body, Put, Delete, Param } from "@nestjs/common";
-import {Todo} from "./todo.model";
 import {CreateTodoDto} from "./dto/create-todo.dto";
 
 @Controller('todos')
@@ -8,32 +7,32 @@ export class TodoController{
 
   constructor(private readonly todoService:TodoService) {}
 
-  @Get('/allTodo')
+  @Get('/all-todo')
   getAllTodo(){
     return this.todoService.getAllTodo();
   }
 
-  @Post('/createTodo')
+  @Post('/create-todo')
   createTodo(@Body() todoDto: CreateTodoDto){
     return this.todoService.createTodo(todoDto);
   }
 
-  @Put('/updateAllTodo')
+  @Put('/update-all-todo')
   updateAllTodo(@Body() todoData: CreateTodoDto, id:number){
     return this.todoService.updateAllTodo(todoData, id);
   }
 
-  @Put('/updateCheckTodo/:id')
+  @Put('/update-check-todo/:id')
   updateCheckTodo(@Param('id') id: number, @Body() todoData: CreateTodoDto){
     return this.todoService.updateCheckTodo(todoData, id);
   }
 
-  @Delete('/deleteOne/:id')
+  @Delete('/delete-one/:id')
   deleteOneTodo(@Param('id') id: number){
     return this.todoService.deleteOneTodo(id);
     }
 
-  @Delete('/deleteaAll')
+  @Delete('/deletea-all')
   deleteAllTodo(@Body() isChecked: boolean){
     return this.todoService.deleteAllTodo(isChecked);
   }
